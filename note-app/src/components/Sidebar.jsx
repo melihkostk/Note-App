a882e6f0c44f7c4aeca9a2e65366063d762a53e9
melihkostk <melih1kostak@gmail.com>
2026-06-25 15:17:46 +0300

    Added some media queries for Header and Sidebar component

--- note-app/src/components/Sidebar.jsx
+++ note-app/src/components/Sidebar.jsx
@@ -5,34 +5,34 @@ import deleteIcon from '../assets/delete.png'
 export function Sidebar({darkMode, sidebarShown, archivePage ,contentShown, setArchivePage, setContentShown ,trashPage, setTrashPage}) {
 
     return (
-        sidebarShown && (<div className='grow shrink-0 basis-auto pt-2 w-70'>
+        sidebarShown && (<div className='grow shrink-0 basis-auto pt-2 w-70 max-[600px]:w-17'>
             <div
                 onClick={() => {
                     setArchivePage(false);
                     setContentShown(true);
                     setTrashPage(false)
                 }}
-                className={`${darkMode ? "text-[#E8EAED] hover:bg-[#282A2C]" : "text-[#202124] hover:bg-[#f1f3f4]"} flex items-center text-sm font-semibold rounded-r-full p-3 ${contentShown ? `${darkMode ? "bg-[#41311C]" : "bg-[#feefc3]"}` : ""} cursor-pointer`}>
+                className={`${darkMode ? "text-[#E8EAED] hover:bg-[#282A2C]" : "text-[#202124] hover:bg-[#f1f3f4]"} flex items-center text-sm font-semibold rounded-r-full p-3 ${contentShown ? `${darkMode ? "bg-[#41311C]" : "bg-[#feefc3]"}` : ""} cursor-pointer max-[600px]:rounded-full max-[600px]:justify-center`}>
                 <img className='px-3' src={bulbIcon} alt="Bulb Icon" />
-                <div className='ml-5'>Notlar</div>
+                <div className='ml-5 max-[600px]:hidden'>Notlar</div>
             </div>
             <div onClick={() => {
                 setArchivePage(true);
                 setContentShown(false);
                 setTrashPage(false)
             }}
-                className={`${darkMode ? "text-[#E8EAED] hover:bg-[#282A2C]" : "text-[#202124] hover:bg-[#f1f3f4]"} flex items-center text-sm font-semibold p-3 ${archivePage ? `${darkMode ? "bg-[#41311C]" : "bg-[#feefc3]"}` : ""} cursor-pointer rounded-r-full`}>
+                className={`${darkMode ? "text-[#E8EAED] hover:bg-[#282A2C]" : "text-[#202124] hover:bg-[#f1f3f4]"} flex items-center text-sm font-semibold p-3 ${archivePage ? `${darkMode ? "bg-[#41311C]" : "bg-[#feefc3]"}` : ""} cursor-pointer rounded-r-full max-[600px]:rounded-full max-[600px]:justify-center`}>
                 <img className='px-3' src={archiveIcon} alt="Archive Icon" />
-                <div className='ml-5'>Arşiv</div>
+                <div className='ml-5 max-[600px]:hidden'>Arşiv</div>
             </div>
             <div onClick={() => {
                 setArchivePage(false);
                 setContentShown(false);
                 setTrashPage(true)
             }} 
-            className={`${darkMode ? "text-[#E8EAED] hover:bg-[#282A2C]" : "text-[#202124] hover:bg-[#f1f3f4]"} flex items-center text-sm font-semibold p-3 ${trashPage ? `${darkMode ? "bg-[#41311C]" : "bg-[#feefc3]"}` : ""} cursor-pointer rounded-r-full`}>
+            className={`${darkMode ? "text-[#E8EAED] hover:bg-[#282A2C]" : "text-[#202124] hover:bg-[#f1f3f4]"} flex items-center text-sm font-semibold p-3 ${trashPage ? `${darkMode ? "bg-[#41311C]" : "bg-[#feefc3]"}` : ""} cursor-pointer rounded-r-full max-[600px]:rounded-full max-[600px]:justify-center`}>
                 <img className='px-3' src={deleteIcon} alt="Archive Icon" />
-                <div className='ml-5'>Çöp Kutusu</div>
+                <div className='ml-5 max-[600px]:hidden'>Çöp Kutusu</div>
             </div>
         </div>)
     )
--- note-app/src/components/Trash.jsx
+++ note-app/src/components/Trash.jsx
@@ -30,14 +30,14 @@ export function Trash({setDeletePShown, setNotes, darkMode, deletedNotes, setDel
     return (
         <div className="flex flex-col items-center justify-center">
             <div className="flex items-center">
-                <h1 className={`${darkMode ? "text-[#E8EAED]" : "text-[#202124]"} text-[17px] italic font-semibold`}>Çöp Kutusu'ndaki notlar 7 gün sonra silinir.</h1>
-                {deletedNotes && <a onClick={() => setDeleteWarning(true)} className={`${deletedNotes.length > 0 ? "block" : "hidden"} px-6 py-2 ml-4 cursor-pointer ${darkMode ? "text-[#8AB4F8]" : "text-[#1A73E8]"} font-semibold text-sm`}>Çöp Kutusunu boşalt</a>}
+                <h1 className={`${darkMode ? "text-[#E8EAED]" : "text-[#202124]"} text-[17px] italic font-semibold text-center`}>Çöp Kutusu'ndaki notlar 7 gün sonra silinir.</h1>
+                {deletedNotes && <a onClick={() => setDeleteWarning(true)} className={`${deletedNotes.length > 0 ? "block" : "hidden"} px-6 py-2 ml-4 cursor-pointer truncate ${darkMode ? "text-[#8AB4F8]" : "text-[#1A73E8]"} font-semibold text-sm`}>Çöp Kutusunu boşalt</a>}
             </div>
             {deletedNotes && deletedNotes.length === 0 && <div className='flex flex-col items-center mt-[20vh]'>
                 <img className='w-30 h-30 object-cover m-5' src={deleteIcon} alt="" />
                 <div className='text-[22px] text-[#9AA0A6]'>Çöp Kutusu'nda not yok</div>
             </div>}
-            {deletedNotes && deletedNotes.length > 0 && <div className="flex items-start gap-3 w-full mt-8 flex-wrap max-w-260">
+            {deletedNotes && deletedNotes.length > 0 && <div className="flex items-start justify-center gap-3 w-full mt-8 flex-wrap max-w-260">
                 {deletedNotes.map((n) => (
                     <Note
                         key={n.id}
